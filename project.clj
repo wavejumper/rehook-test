5509cca282be694e798120babacfc6b360299d69
Thomas Crowley <crowley@kibu.com.au>
2019-11-23 15:06:20 +1100

    shadow-cljs works with lein

--- project.clj
+++ project.clj
@@ -10,9 +10,12 @@
    [zprint "0.5.3"]]
 
   :profiles
-  {:dev {:dependencies [[thheller/shadow-cljs "2.8.52"]
-                        [binaryage/devtools "0.9.11"]]
-         :source-paths ["src" "dev" "test"]}}
+  {:cljs {:dependencies [[thheller/shadow-cljs "2.8.52"]
+                         ;; https://github.com/thheller/shadow-cljs/issues/488
+                         [com.google.javascript/closure-compiler-unshaded "v20190325"]
+                         [org.clojure/google-closure-library "0.0-20190213-2033d5d9"]
+                         [binaryage/devtools "0.9.11"]]
+          :source-paths ["src" "dev" "test"]}}
 
   :source-paths
   ["src"])
